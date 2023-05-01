b3416c8e0a4bccdd8a74706e69f2cdfac5e32c6f
jamesnascimento1994 <james.nascimento94@yahoo.com>
2023-04-30 22:36:34 -0400

    added solutions for triangular and simple join with count

--- challenges.sql
+++ challenges.sql
@@ -12,3 +12,25 @@ SELECT price * amount AS total FROM items
 -- Challenge Source: https://www.codewars.com/kata/52fba66badcd10859f00097e
 
 SELECT str, translate(str, 'aeiouAEIOU', '') AS res FROM disemvowel;
+
+-- Challenge Three: Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e. You need to return the nth triangular number. You should return 0 for out of range values.
+-- Challenge Source: https://www.codewars.com/kata/525e5a1cb735154b320002c8
+
+SELECT n, 
+  CASE
+    WHEN n <= 0 THEN 0
+    ELSE (CAST(n AS INT) * (n + 1)) / 2
+  END AS res
+  FROM triangular;
+
+-- Challenge Four: For this challenge you need to create a simple SELECT statement that will return all columns from the people table, and join to the toys table so that you can return the COUNT of the toys.
+-- Challenge Source: https://www.codewars.com/kata/580918e24a85b05ad000010c
+
+SELECT
+  p.id,
+  p.name,
+  COUNT(t.name) AS toy_count
+FROM people AS p 
+LEFT JOIN toys AS t
+ON p.id = t.people_id
+GROUP BY p.id, p.name;
